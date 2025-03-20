ff8e26ad44aceee9295a5d69970c3453d6a03fb2
Sabrino01 <rasolonjatovohajaniain@gmail.com>
2025-03-20 14:05:18 +0300

    configuration and update vue 2 on vue 3, install @vite and test vue 3 with a simple login.vue

---
+++ resources/js/App.vue
@@ -0,0 +1,9 @@
+<template>
+    <div>
+        <Login />
+    </div>
+</template>
+
+<script setup>
+import Login from "./components/Login.vue";
+</script>
--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,6 @@
-require('./bootstrap');
+import { createApp } from "vue";
+import App from "./App.vue";
+import "./bootstrap";
+import "../css/app.css";
+
+createApp(App).mount("#app");
